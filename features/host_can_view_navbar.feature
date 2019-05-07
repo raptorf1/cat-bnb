63bddb66a571ab125dd1b93948ccb4f49c617c5b
stefan_karlberg <stefan.karlberg@smackit.se>
2019-05-07 09:19:17 +0200

    Added new tests, all green. Added tailwind css. Adjusted the nevigation bar with welcome-message, and slogan under logo.

--- features/host_can_view_navbar.feature
+++ features/host_can_view_navbar.feature
@@ -3,7 +3,13 @@ Feature: Host can view navbar and see "Become a Host" link
     In order to use the service
     I would like to navigate the site
 
+  @javascript
   Scenario: Visitor can see the landing page
     When I visit the "landing page"
-    # Then I should see a "navbar"
-    # And I should see link "Become a host" 
+    Then I should see a navigation bar
+    And I should see "Welcome Admin"
+    And I should see "BECOME A HOST" link 
+    And I click "BECOME A HOST" link
+ 
+    
+    
--- features/step_definitions/basics_steps.rb
+++ features/step_definitions/basics_steps.rb
@@ -2,10 +2,19 @@ When("I visit the {string}") do |string|
   visit root_path
 end
 
-# Then("I should see a {string}") do |string|
-#   expect(root_path).to have_content(string)
-# end
+Then("I should see {string}") do |text|
+  expect(page).to have_content text
+end
 
-# Then("I should see link {string}") do |string|
-#   page.should have_link(string)
-# end
+Then("I should see a navigation bar") do
+  find('.nav_bar').visible?
+end
+
+Then("I should see {string} link") do |string|
+  page.should have_link(string)
+end
+
+When("I click {string} link") do |string|
+  click_link string
+end
+
