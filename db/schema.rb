8d7555d2c2b7e7d5b689e7ed210510274c83175d
zanenkn <zane.neikena@gmail.com>
2019-05-08 20:31:18 +0200

    Adds columns to the Profile db: name, location, price, description, picture. Migrates the db.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_08_144820) do
+ActiveRecord::Schema.define(version: 2019_05_08_182912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,11 @@ ActiveRecord::Schema.define(version: 2019_05_08_144820) do
     t.string "create"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "location"
+    t.integer "price"
+    t.string "description"
+    t.string "picture"
   end
 
   create_table "users", force: :cascade do |t|
