6e2603213b3bb358233fe10752f5c87a4b30d358
zanenkn <zane.neikena@gmail.com>
2019-05-08 19:18:25 +0200

    Creates a Profile model, defines the 'create' as a resource in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root controller: :listings, action: :index
   resources :listings, only: [:new, :create]
+  resources :profiles, only: [:new]
 end
