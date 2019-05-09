30e22786edea3cd5b04cfa402acd74117f7dc4c2
raptorf1 <raptor_f1@hotmail.com>
2019-05-09 14:10:20 +0200

    Defines the 'create' action on the Profiles controller and opens the relevant route. Happy path steps of scenario all passing.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,23 @@
 class ProfilesController < ApplicationController
+  
   def new
+    @profile = Profile.new
   end
+
+  def create
+    profile = Profile.create(profile_params)
+
+    if profile.persisted?
+      redirect_to root_path, notice: "You have successfully created a host profile. Now you can apply to host cats."
+    else
+      redirect_to new_profile_path, notice: "Please fill in all fields"
+    end
+  end
+
+  private
+  
+  def profile_params
+    params.require(:profile).permit(:name, :location, :description, :picture)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'profiles/new'
   devise_for :users
+
   root controller: :listings, action: :index
+
   resources :listings, only: [:new, :create]
-  resources :profiles, only: [:new]
+
+  resources :profiles, only: [:new, :create]
+
 end
